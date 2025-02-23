2fb5b8945f68f6119e04f55835020df427aa3447
ArchiMoebius <poerhiz@gmail.com>
2025-02-23 10:15:22 -0500

    bump to ubuntu 24.04.02

--- build.sh
+++ build.sh
@@ -19,8 +19,8 @@ ssh_nat_port=$((RANDOM % range + min_port))
 
 pushd $BASEDIR
     cat > Ubuntu24_LTS_64.auto.pkrvars.hcl << EOF
-iso_url = "https://releases.ubuntu.com/noble/ubuntu-24.04.1-desktop-amd64.iso"
-iso_checksum = "sha256:c2e6f4dc37ac944e2ed507f87c6188dd4d3179bf4a3f9e110d3c88d1f3294bdc"
+iso_url = "https://releases.ubuntu.com/noble/ubuntu-24.04.2-desktop-amd64.iso"
+iso_checksum = "sha256:d7fe3d6a0419667d2f8eff12796996328daa2d4f90cd9f87aa9371b362f987bf"
 headless = false
 vm_name = "${VM_NAME}"
 ssh_nat_port = ${ssh_nat_port}
